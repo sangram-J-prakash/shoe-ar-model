779df356a68e00d3577b62c971c9f302aa313375
Sangram <sangramjpgiri.07019@gmail.com>
2024-07-26 17:16:08 +0530

    new file created

--- src/ARViewer.jsx
+++ src/ARViewer.jsx
@@ -1,153 +1,82 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import '@google/model-viewer';
-import model from "../src/basketball_shoe.glb"; 
-import modelios from "../src/Basketball_Shoe.usdz";
+import Eiffel_tower from "../src/Eiffel_tower.glb";
+import Eiffel_towerios from "../src/Eiffel_tower.usdz";
 import logo from "../src/hdr/Logo.png"
+import logo2 from "../src/logo-paris.png"
 import hdri from "../src/hdr/illovo_beach_balcony_4k.hdr"
-import { Box, Camera,Forward, Fullscreen, Minimize, X } from 'lucide-react';
-import "../src/App.css"
+import { Box } from 'lucide-react';
+import "../src/App.css";
+import { MoonLoader } from 'react-spinners';
 
 const ARViewer = () => {
   const viewerRef = useRef(null);
-  const [fullscreen, setfullscreen] = useState(false);
-  const [open, setopen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const handleFullScreen = () => {
-    setfullscreen(i => !i)
-    if (viewerRef.current) {
-      const element = viewerRef.current;
-
-      if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
-        // If no element is in fullscreen, enter fullscreen
-        if (element.requestFullscreen) {
-          element.requestFullscreen();
-        } else if (element.mozRequestFullScreen) { // Firefox
-          element.mozRequestFullScreen();
-        } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
-          element.webkitRequestFullscreen();
-        } else if (element.msRequestFullscreen) { // IE/Edge
-          element.msRequestFullscreen();
-        }
-      } else {
-        // Otherwise, exit fullscreen
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.mozCancelFullScreen) { // Firefox
-          document.mozCancelFullScreen();
-        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
-          document.webkitExitFullscreen();
-        } else if (document.msExitFullscreen) { // IE/Edge
-          document.msExitFullscreen();
-        }
-      }
-    }
-  };
-
-
-  const handleCaptureImage = () => {
-    if (viewerRef.current) {
-      const imgDataUrl = viewerRef.current.toDataURL();
-
-      const link = document.createElement('a');
-      link.href = imgDataUrl;
-      link.download = 'captured_image.png';
-
-      document.body.appendChild(link);
-      link.click();
-
-      document.body.removeChild(link);
-    }
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
 
   return (
-    <model-viewer
-      id="color"
-      ref={viewerRef}
-      autoplay
-      ar
-      ar-modes="webxr scene-viewer quick-look"
-      // ar-scale="fixed"
-      camera-orbit="-30deg auto auto"
-      max-camera-orbit="auto 100deg auto"
-      shadow-intensity="2"
-      camera-controls
-      touch-action="pan-y"
-      poster="/public/logo192.png"
-      src={model}
-      ios-src={modelios} // ios src
-      auto-rotate
-      style={{ width: '100%', height: '100vh' }}
-      // exposure="1.7"
-      shadow-softness="0"
-      // tone-mapping="linear"
-      // tone-mapping="aces"
-      tone-mapping="filmic"
-      // tone-mapping="commerce"
-      // tone-mapping="hejl"
-      // tone-mapping="uncharted2"
-      class="container_div"
-      ar-status="presenting"
-      ar-placement="floor"
-      // ar-placement="auto"
-      interaction-prompt="auto"
-      interaction-prompt-style="wiggle"
-      min-camera-orbit="auto 0deg auto"
-      max-field-of-view="45deg"
-      min-field-of-view="10deg"
-      // bounds="tight"
-      bounds="auto"
-      // skybox-image={hdri}
-      environment-image={hdri}
-      alt="AR Model"
-    >
-      <img className='position-absolute top-0 start-0 z-3 mt-2 ms-2 bg-dark-subtle rounded px-2' alt='logo' src={logo} width={100} />
-      <div className='position-absolute transition-element end-0 top-50 rounded d-flex flex-column translate-middle-x z-3 bg-dark-subtle'>
-        <span
-          className='m-1 p-1 border rounded-2 pointer'
-          onClick={() => { setopen(!open) }}
-        >
-          {open ? <X className='transition-element' /> : <X className='transition-element'  style={{ rotate: '-135deg' }}/>}
-        </span>
-        {
-          open && <>
-            <span
-              className='m-1 transition-element p-1 border rounded-2 pointer'
-              onClick={handleFullScreen}
-            >
-              {fullscreen ? <Minimize /> : <Fullscreen xlinkTitle='fullscreen' />}
-            </span>
-            <span
-              className='m-1 transition-element p-1 border rounded-2 pointer'
-              onClick={handleCaptureImage}
-            >
-              <Camera />
-            </span>
-            <span
-              className='m-1 transition-element p-1 border rounded-2 pointer'
-            // onClick={handleCaptureImage}
-            >
-              <Forward />
-            </span>
-          </>
-        }
-      </div>
-      <model-viewer-lights
+    loading ? <Loadingcomp /> : (
+      <model-viewer
+        id="color"
+        ref={viewerRef}
+        autoplay
+        ar
+        // ar-modes="webxr scene-viewer quick-look"
+        ar-modes="scene-viewer quick-look"
+        camera-orbit="-30deg auto auto"
+        max-camera-orbit="auto 100deg auto"
         shadow-intensity="2"
-        environment-image={hdri}
-        shadow-softness="0.55"
-      />
-      <button slot="ar-button" className='position-absolute btn-16 px-4 border py-2 border-2 shadow z-3 rounded pointer bottom-0 start-50 translate-middle'>
-        <Box /> AR View
-      </button>
-      <button className='position-absolute btn-16 px-4 border py-2 border-2 shadow rounded pointer bottom-0 start-50 translate-middle'>
-        <Box /> AR View
-      </button>
-      {/* <span slot="ar-button" className='pointer position-absolute bottom-0 start-50 bg-black '>
-      <Box /> AR 
-      </span> */}
-    </model-viewer>
+        camera-controls
+        touch-action="pan-y"
+        poster="/public/logo192.png"
+        src={Eiffel_tower}
+        ios-src={Eiffel_towerios}
+        // auto-rotate
+        style={{ width: '100%', height: '100vh' }}
+        shadow-softness="0"
+        tone-mapping="filmic"
+        class="container_div"
+        ar-placement="floor"
+        interaction-prompt="auto"
+        interaction-prompt-style="wiggle"
+        min-camera-orbit="auto 0deg auto"
+        max-field-of-view="45deg"
+        min-field-of-view="10deg"
+        bounds="auto"
+        alt="AR Model"
+      >
+        <a href="https://realitiqxr.com/" rel="noreferrer" target='_blank'>
+        <img style={{ maxWidth: "120px" }} className='position-absolute bg-dark-subtle pointer top-0 start-0 z-3 ms-2 p-0 mt-1 rounded' alt='logo' src={logo} />
+      </a>
+        <img style={{ maxWidth: "120px" }} className='position-absolute bg-dark-subtle pointer top-0 end-0 z-3 me-2 p-0 mt-1 rounded' alt='logo' src={logo2} />
+        <model-viewer-lights
+          shadow-intensity="2"
+          environment-image={hdri}
+          shadow-softness="0.55"
+        />
+        <button slot="ar-button"
+          className='position-absolute bipping-button px-4 py-2 z-3 rounded pointer bottom-0 start-50 mb-4 translate-middle-x'>
+          <Box /> AR View
+        </button>
+      </model-viewer>
+    )
   );
 };
 
 export default ARViewer;
+
+const Loadingcomp = () => (
+  <div style={{backgroundColor:"#4d5254"}} className='vh-100 text-black d-flex justify-content-center align-items-center'>
+    <a href="https://realitiqxr.com/" rel="noreferrer" target='_blank'>
+        <img style={{ maxWidth: "120px" }} className='position-absolute bg-dark-subtle pointer top-0 start-0 z-3 ms-2 p-0 mt-1 rounded' alt='logo' src={logo} />
+      </a>
+      <img style={{ maxWidth: "120px" }} className='position-absolute bg-dark-subtle pointer top-0 end-0 z-3 me-2 p-0 mt-1 rounded' alt='logo' src={logo2} />
+    <MoonLoader color="#6200ea" loading={true} size={150} />
+  </div>
+);
